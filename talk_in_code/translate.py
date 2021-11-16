781304cdf8f9f2fbb7f5bcb3a441d89abf5373f0
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-11-15 22:35:05 -0300

    add 2 new features - create and call functions | adapt translate.py to receive them

--- talk_in_code/translate.py
+++ talk_in_code/translate.py
@@ -6,6 +6,10 @@ def from_pt(natural_text: str):
         code = pt_translator.print_structure(natural_text)
     elif natural_text.split(' ')[0] == 'variável':
         code = pt_translator.declare_variable(natural_text)
+    elif natural_text.split(' ')[0] == 'defina':
+        code = pt_translator.define_function(natural_text)
+    elif natural_text.split(' ')[0] == 'chame':
+        code = pt_translator.call_function(natural_text)
     return code
 
 
@@ -16,5 +20,5 @@ def run(natural_text: str, language='pt-BR'):
 
 
 if __name__ == '__main__':
-    code_line = run('variável x recebe string itanu')
+    code_line = run('chame this function parâmetros inteiro number string algo')
     print(code_line)
